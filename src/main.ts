ad19a83c3c12f235d126214999ab59ffeadbea7f
Arnur <arnurarykbayev@mail.ru>
2023-07-02 16:41:34 +0600

    fist commit

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import './assets/main.css'
+import { createPinia } from 'pinia';
+import App from './App.vue'
+
+const pinia = createPinia();
+
+createApp(App).use(pinia).mount('#app')
