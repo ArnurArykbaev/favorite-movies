491e47972c6e2fd11b660a9990a30c609ac77a34
Arnur <arnurarykbayev@mail.ru>
2023-07-06 01:09:36 +0600

    add state to localStorage

--- src/stores/MovieStore.ts
+++ src/stores/MovieStore.ts
@@ -1,13 +1,18 @@
 import { defineStore } from "pinia";
 import { Movie } from "../models/movie";
-import { ref, computed } from "vue";
+import { ref, computed, watch } from "vue";
 
 export const useMovieStore = defineStore("MovieStore", () => {
     const movies = ref([] as Movie[]);
     const activeTab = ref(2);
 
+    const moviesInLocalStorage = localStorage.getItem("movies")
+    if (moviesInLocalStorage) {
+        movies.value = JSON.parse(moviesInLocalStorage)._value;
+    }
+
     const watchedMovies = computed((): Movie[] => movies.value.filter(el => el.isWatched));
-    const totalCountMovies = computed((): number  => movies.value.length)
+    const totalCountMovies = computed((): number => movies.value.length)
 
     const setActiveTab = (id: number) => activeTab.value = id;
     const toggleWatched = (id: number) => {
@@ -15,6 +20,10 @@ export const useMovieStore = defineStore("MovieStore", () => {
         movies.value[idx].isWatched = !movies.value[idx].isWatched;
     }
     const deleteMovie = (id: number) => movies.value = movies.value.filter(el => el.id !== id);
+
+    watch(() => movies, (state) => {
+        localStorage.setItem("movies", JSON.stringify(state))
+    }, { deep: true })
 
     return {
         movies, activeTab, setActiveTab, watchedMovies, totalCountMovies, toggleWatched, deleteMovie
